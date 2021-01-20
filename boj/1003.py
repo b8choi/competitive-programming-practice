0e8a758822f57b83badae2e9daa06780f1fd2a6d
Byeongmin Choi <28807922+b8choi@users.noreply.github.com>
2021-01-21 04:03:13 +0900

    1003번 풀이 추가

--- boj/1003.py
+++ boj/1003.py
@@ -1,7 +1,13 @@
-def fibo(n):
-    
-
 t = int(input())
 for i in range(t):
     n = int(input())
-    fibo(n)
+
+    cnt0 = [0 for i in range(max(2, n + 1))]
+    cnt0[0] = 1
+    cnt1 = [0 for i in range(max(2, n + 1))]
+    cnt1[1] = 1
+    for j in range(2, n + 1):
+        cnt0[j] = cnt0[j - 1] + cnt0[j - 2]
+        cnt1[j] = cnt1[j - 1] + cnt1[j - 2]
+
+    print(cnt0[n], cnt1[n])
