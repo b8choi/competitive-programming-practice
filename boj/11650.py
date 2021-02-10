1d8612284ec4505c00fbc4c549880fb7488e01c4
Byeongmin Choi <28807922+b8choi@users.noreply.github.com>
2021-02-10 17:49:29 +0900

    11650번 풀이 추가

---
+++ boj/11650.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+points = []
+for i in range(n):
+    point = [0, 0]
+    point[0], point[1] = [int(x) for x in input().split()]
+    points.append(point)
+
+points.sort(key=lambda x: x[1])
+points.sort(key=lambda x: x[0])
+
+for p in points:
+    print(p[0], p[1])
