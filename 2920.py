0561149ce5cce85be65c7ed72542e3bd1ce3a6ee
Byeongmin Choi <28807922+b8choi@users.noreply.github.com>
2021-03-13 23:58:53 +0900

    2920번 풀이 추가

---
+++ 2920.py
@@ -0,0 +1,8 @@
+tones = [int(x) for x in input().split()]
+
+if tones == [1, 2, 3, 4, 5, 6, 7, 8]:
+    print('ascending')
+elif tones == [8, 7, 6, 5, 4, 3, 2, 1]:
+    print('descending')
+else:
+    print('mixed')
